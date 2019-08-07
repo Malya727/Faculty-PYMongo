6d2045ae646e6434e0d7fb56d1e94e5948598633
niranjanmalya <niranjanmalya5@gmail.com>
2019-08-07 12:30:37 +0530

    Project

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import database as db 
 
 
-print("~ "*10,"Welcome to Faculty Management System"," ~"*10)
+print("~ "*10,"Welcome to Faculty Management System","~ "*10,"\n")
 while True:
-    print("\n1.Add Faculty\n2.Add Subject To Faculty\n3.Remove Subject")
-    choice = int(input("Enter Your Choice : "))
+    print("~ "*15,"\n1.Add Faculty\n2.Add Subject To Faculty\n3.Remove Subject From Faculty\n4.Increase Experience by one year\n5.Update Faculty Qualification\n6.Delete Faculty\n7.Faculty With Subject\n8.Exit\n","~ "*15)
+    choice = int(input("\nEnter Your Choice : "))
 
     if choice == 1:
         db.add_faculty()
@@ -12,5 +12,13 @@ while True:
         db.add_ex_subject()
     elif choice == 3:
         db.delete_one_subject()
+    elif choice == 4:
+        db.increment_exp()
+    elif choice == 5:
+        db.update_qualification()
+    elif choice == 6:
+        db.delete_faculty()
+    elif choice == 7:
+        db.faculty_subject()
     else:
         print("Invalid Choice Please Try Again.....")
--- database.py
+++ database.py
@@ -1,56 +1,157 @@
-import pymongo 
+import pymongo
+from prettytable import PrettyTable 
   
-try: 
-    conn = pymongo.MongoClient("mongodb://localhost:27017/")
+try:
+    url = "mongodb+srv://user:user@malya-pmnjq.mongodb.net/test?retryWrites=true&w=majority"
+    conn = pymongo.MongoClient(url)
     db = conn['mite'] 
     collec = db['faculty']
     print("Connected successfully........\n") 
-except:   
-    print("Could not connect to MongoDB........\n")
+except Exception as e:   
+    print(e)
+
+
+#adding new faculty into db
 
 def add_faculty():
     #reading Data From User
 
     fac = {}
-    sub = {}
     name = input("Enter Name :")
     age = input("Enter Age :")
     gender = input("Enter Gender :")
-    su = input("Enter Subject Name :")
-    ho = int(input("Enter Teaching Hour :"))
-    exp = input("Enter Experience :")
+    s_name = input("Enter Subject Name :")
+    s_time = int(input("Enter Teaching Hour :"))
+    exp = int(input("Enter Experience :"))
     type_fac = input("Enter Type of employee :")
     qualification = input("Enter qualification :")
 
     #Adding to dictonary
 
-    fac['Name'] = name
-    fac['age'] = age
-    fac['gender'] = gender
-    fac['subjects'] = [su,ho]
-    fac['experience'] = exp
-    fac['type'] = type_fac
-    fac['qualification'] = qualification
+    fac={
+            "name" : name,
+            "age" : age,
+            "gender" : gender,
+            "subjects" :[
+                {
+                  "s_title" : s_name,
+                  "s_time" : s_time
+                }],
+            "experience" : exp,
+            "type" : type_fac,
+            "qualification" : qualification
+        }
 
     collec.insert_one(fac)
     print("Faculty Record Inserted Successfully.......")
 
+    print("+ "*40)
+
+
+
+#adding one more subject for exsisting faculty
+
 def add_ex_subject():
-    sub = {}
+    try:
+        sub = {}
 
-    name = input("Enter Faculty Name To Add Subjects :")
-    
-    su = input("Enter Subject Name :")
-    ho = int(input("Enter Teaching Hour :"))
-    sub['Item'] = su
-    sub['hour'] = ho
+        name = input("Enter Faculty Name To Add Subjects :")
+        
+        su = input("Enter Subject Name :")
+        ho = int(input("Enter Teaching Hour :"))
+        sub = {
+            "s_title" : su,
+            "s_time" : ho
+        }
 
-    collec.update_one({"name":name},{"$push" : { "subjects" : sub}})
+        collec.update_one({"name":name},{"$push" : { "subjects" : sub}})
 
-    print(f"\nFaculty By Name {name} is updated by With 1 new Subjects")
+        print(f"\nFaculty By Name {name} is updated by With 1 new Subjects")
 
+    except:
+        print(f"Unable to find employee with name {name}")
+
+    print("+ "*40)
+
+
+#deleting exisisting Subject From Faculty
 def delete_one_subject():
-    pass
+    try:
+        name = input("Enter Faculty Name To Remove Subjects :")
+        sub = input("Enter The Subject Name To remove :")
+
+        myquery = { "name": name }
+        newvalues = { "$pull": { "subjects" : {"s_title" : sub} } }
+
+        collec.update_one(myquery,newvalues)
+    except:
+        print("Unable To Delete Subject From {name} ")
+
+    print("+ "*40)
+
+
+#incrementing FAculty Experience by one year
+def increment_exp():
+
+    name = input("Enter The Name Of the Faculty To Incerment His/Her Experience :")
+
+    try:
+        myquery = { "name": name }
+        newvalues = { "$inc": { "experience":1  } }
+        collec.update_one(myquery,newvalues)
+        print(f"Faculty With Name {name} increased His Experience by one year")
+    except :
+        print(f"Unable to find employee With Name : {name}")
+
+    print("+ "*40)
+
+
+#updating Faculty Qualification With new Qualification
+def update_qualification():
+    name = input("Enter The Name Of the Faculty To Update His/Her Qualification :")
+
+    try:
+        qual = input("Enter New Qualification to update Faculty :")
+        myquery = { "name": name}
+        newvalues = { "$set": { "qualification": qual } }
+
+        collec.update_one(myquery, newvalues)
+
+        print(f"Faculty With Name {name} is updated with New Qualification {qual} successfully!")
+
+    except:
+        print(f"Unable to Update Faculty With Name : {name} ")
+
+    print("+ "*40)
+
+#deleting Faculty From DB
+
+def delete_faculty():
+    name = input("Enter The Name Of the Faculty To Delete His Details :")
+
+    try:
+        myquery = { "name": name }
+        collec.delete_one(myquery)
+        print(f"Faculty With Name {name} deleted Successfully")
+    except:
+        print(f"Unable to Delete Faculty With name : {name} ")
+
+    print("+ "*40)
+
+def faculty_subject():
+    que = [{"$unwind" : "$subject"},{"$project" : {"name" : 1 ,"subjects" : "$subjects.s_title"}}]
+
+    x = PrettyTable()
+    x.field_names = ["Name","Subject"]
+
+    res = collec.aggregate(que)
+
+
+    for r in res:
+        x.add_row([r['name'],r['subjects']])
+
+    print(x)
+           
 
 
 
